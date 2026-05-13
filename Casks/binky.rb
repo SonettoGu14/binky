bb9fde0ae8b6ff209709bf997b068bb996492684
Derek J <127624400+heyderekj@users.noreply.github.com>
2026-05-13 10:02:06 -0500

    Bump to v1.5.1

--- Casks/binky.rb
+++ Casks/binky.rb
@@ -1,8 +1,8 @@
 cask "binky" do
-  version "1.5.0"
+  version "1.5.1"
   # Refresh this checksum when publishing `Binky-{version}.zip` via `./release.sh`
   # (a local ad-hoc build can produce a different hash than the signed release artifact).
-  sha256 "ae4beed5d71741a5a4230fadadecf7df920305a687a28f2e3980c9194808a919"
+  sha256 "95df8b250cc64385ff6d8b2a55d0518ac9c061758c73b778e608fdce739ebb7a"
 
   url "https://github.com/heyderekj/binky/releases/download/v#{version}/Binky-#{version}.zip",
       verified: "github.com/heyderekj/binky/"
